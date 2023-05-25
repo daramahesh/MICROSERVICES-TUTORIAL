675bfb1c7bc60992b6d90ad048740dd358ee3553
daramahesh <121857049+daramahesh@users.noreply.github.com>
2023-05-25 11:42:52 +0530

    Update AuthController.java

--- ApiGateway/src/main/java/org/gateway/controller/AuthController.java
+++ ApiGateway/src/main/java/org/gateway/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+// These changes are made in git
 @RestController
 @RequestMapping("/auth")
 public class AuthController {

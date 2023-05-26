267b5b0bd4528960715c845637f259de91dfa2aa
daramahesh <121857049+daramahesh@users.noreply.github.com>
2023-05-26 18:55:30 +0530

    Delete HotelService directory

--- HotelService/src/main/java/org/hotel/service/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.hotel.service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String s) {
-    }
-
-    public ResourceNotFoundException() {
-    }
-}
--- HotelService/src/main/java/org/hotel/service/repositories/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package org.hotel.service.repositories;
-
-import org.hotel.service.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
